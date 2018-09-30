5af10737e240954c6cf423504094fbb8612034b2
Yodsavee <vee_535@hotmail.com>
2018-09-30 17:05:19 +0700

    Finish Range_insert

--- Nattee/Range_insert/student.h
+++ Nattee/Range_insert/student.h
@@ -5,7 +5,18 @@ template <typename T>
 void CP::vector<T>::insert(iterator position,iterator first,iterator last) {
   std::vector<T> temp(position,end());
   std::vector<T> ins(first,last);
-  
+  int pos = position - begin();
+  int siz = last - first;
+  ensureCapacity(mSize+siz);
+  for(auto x:ins){
+    mData[pos] = x;
+    pos++;
+  }
+  for(auto y:temp){
+    mData[pos] = y;
+    pos++;
+  }
+  mSize += siz;
 }
 
 #endif
